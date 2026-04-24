604490b0a9ccfe88815319e99a58fc01410d792c
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-24 22:37:35 +0300

    ST-DTO: Исправлен формат даты

--- stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHit.java
+++ stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHit.java
@@ -26,6 +26,6 @@ public class EndpointHit {
     private String ip;
 
     @NotNull
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
