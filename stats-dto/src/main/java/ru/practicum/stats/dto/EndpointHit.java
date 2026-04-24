46256a2bbeb38f15329222602e79811d479e1809
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-24 20:58:23 +0300

    ST-DTO: Добавлен класс [EndpointHit] для хранения данных о запросе

---
+++ stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHit.java
@@ -0,0 +1,31 @@
+package ru.practicum.stats.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHit {
+
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+}
