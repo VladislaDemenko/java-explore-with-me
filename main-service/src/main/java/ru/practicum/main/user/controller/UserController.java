3690edf826febbdd1a60b2e68974d1cc533b85e6
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-25 19:01:25 +0300

    M-SV-US: Реализован класс UserController с эндпоинтами [GET/POST/DELETE /admin/users]

---
+++ main-service/src/main/java/ru/practicum/main/user/controller/UserController.java
@@ -0,0 +1,44 @@
+package ru.practicum.main.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.user.dto.NewUserRequest;
+import ru.practicum.main.user.dto.UserDto;
+import ru.practicum.main.user.service.UserService;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDto> getUsers(
+            @RequestParam(required = false) List<Long> ids,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size) {
+        log.info("GET /admin/users - ids={}, from={}, size={}", ids, from, size);
+        return userService.getUsers(ids, from, size);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto createUser(@Valid @RequestBody NewUserRequest newUserRequest) {
+        log.info("POST /admin/users - {}", newUserRequest);
+        return userService.createUser(newUserRequest);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long userId) {
+        log.info("DELETE /admin/users/{}", userId);
+        userService.deleteUser(userId);
+    }
+}
