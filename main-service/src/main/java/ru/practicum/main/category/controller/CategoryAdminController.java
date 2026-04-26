785cbaf07bd26fdd7ac9f3e926d0264630832e7c
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:35:29 +0300

    M-SV-CG: Добавлены @Validated в CategoryAdminController

--- main-service/src/main/java/ru/practicum/main/category/controller/CategoryAdminController.java
+++ main-service/src/main/java/ru/practicum/main/category/controller/CategoryAdminController.java
@@ -3,6 +3,7 @@ package ru.practicum.main.category.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.main.category.dto.CategoryDto;
 import ru.practicum.main.category.dto.NewCategoryDto;
@@ -14,6 +15,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/admin/categories")
 @RequiredArgsConstructor
+@Validated
 public class CategoryAdminController {
 
     private final CategoryService categoryService;
