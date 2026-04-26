22278f430824ec027368d1815ed73c02875bd593
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 16:56:57 +0300

    M-SV-EV: Реализован класс для обновления события админом

---
+++ main-service/src/main/java/ru/practicum/main/event/dto/UpdateEventAdminRequest.java
@@ -0,0 +1,22 @@
+package ru.practicum.main.event.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateEventAdminRequest {
+    String annotation;
+    Long category;
+    String description;
+    String eventDate;
+    LocationDto location;
+    Boolean paid;
+    Integer participantLimit;
+    Boolean requestModeration;
+    String stateAction;
+    String title;
+}
