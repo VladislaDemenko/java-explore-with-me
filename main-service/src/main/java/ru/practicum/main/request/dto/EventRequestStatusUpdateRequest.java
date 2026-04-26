3e2d2ef71a956022bf1fc8be39645723f5984505
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:02:30 +0300

    M-SV-RQ: Реализован класс для изменения статуса запросов

---
+++ main-service/src/main/java/ru/practicum/main/request/dto/EventRequestStatusUpdateRequest.java
@@ -0,0 +1,16 @@
+package ru.practicum.main.request.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EventRequestStatusUpdateRequest {
+    private List<Long> requestIds;
+    private String status;
+}
