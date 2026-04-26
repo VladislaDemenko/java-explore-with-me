5c10222eb5cb40af8d7d0c374d4ac9ed5e0ddc0c
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 16:50:24 +0300

    M-SV-CTG: Реализован CategoryPublicController с [GET /categories]

---
+++ main-service/src/main/java/ru/practicum/main/compilation/controller/CompilationPublicController.java
@@ -0,0 +1,33 @@
+package ru.practicum.main.compilation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.compilation.dto.CompilationDto;
+import ru.practicum.main.compilation.service.CompilationService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/compilations")
+@RequiredArgsConstructor
+public class CompilationPublicController {
+
+    private final CompilationService compilationService;
+
+    @GetMapping
+    public List<CompilationDto> getCompilations(
+            @RequestParam(required = false) Boolean pinned,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size) {
+        log.info("GET /compilations - pinned={}, from={}, size={}", pinned, from, size);
+        return compilationService.getCompilations(pinned, from, size);
+    }
+
+    @GetMapping("/{compId}")
+    public CompilationDto getCompilation(@PathVariable Long compId) {
+        log.info("GET /compilations/{}", compId);
+        return compilationService.getCompilation(compId);
+    }
+}
