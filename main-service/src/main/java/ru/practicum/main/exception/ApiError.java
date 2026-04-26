9ea2e094b876d6bd8572f849dc0b381e96bab346
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:33:50 +0300

    M-SV-EX: добавлены @JsonInclude(NON_NULL) для исключения null полей

--- main-service/src/main/java/ru/practicum/main/exception/ApiError.java
+++ main-service/src/main/java/ru/practicum/main/exception/ApiError.java
@@ -1,5 +1,6 @@
 package ru.practicum.main.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiError {
     List<String> errors;
