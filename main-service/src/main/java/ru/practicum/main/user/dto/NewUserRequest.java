2f04d3560e8239efb32c7450f5dc625274111309
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:34:33 +0300

    M-SV-US: Добавлены сообщения об ошибках в аннотации валидации NewUserRequest

--- main-service/src/main/java/ru/practicum/main/user/dto/NewUserRequest.java
+++ main-service/src/main/java/ru/practicum/main/user/dto/NewUserRequest.java
@@ -1,24 +1,25 @@
 package ru.practicum.main.user.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class NewUserRequest {
-    @NotBlank
+    @NotBlank(message = "Field: name. Error: must not be blank. Value: null")
     @Size(min = 2, max = 250)
-    String name;
+    private String name;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Field: email. Error: must not be blank. Value: null")
+    @Email(message = "Field: email. Error: must be a well-formed email address")
     @Size(min = 6, max = 254)
-    String email;
+    private String email;
 }
