7c7bf72e9ce4af5277bb15c5b4a9b5056ec152ac
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 16:49:51 +0300

    M-SV-CTG: Реализован CategoryAdminController с эндпоинтами [POST/PATCH/DELETE /admin/categories]

---
+++ main-service/src/main/java/ru/practicum/main/compilation/controller/CompilationAdminController.java
@@ -0,0 +1,42 @@
+package ru.practicum.main.compilation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.compilation.dto.CompilationDto;
+import ru.practicum.main.compilation.dto.NewCompilationDto;
+import ru.practicum.main.compilation.dto.UpdateCompilationRequest;
+import ru.practicum.main.compilation.service.CompilationService;
+
+import jakarta.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/compilations")
+@RequiredArgsConstructor
+public class CompilationAdminController {
+
+    private final CompilationService compilationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompilationDto createCompilation(@Valid @RequestBody NewCompilationDto newCompilationDto) {
+        log.info("POST /admin/compilations - {}", newCompilationDto);
+        return compilationService.createCompilation(newCompilationDto);
+    }
+
+    @DeleteMapping("/{compId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompilation(@PathVariable Long compId) {
+        log.info("DELETE /admin/compilations/{}", compId);
+        compilationService.deleteCompilation(compId);
+    }
+
+    @PatchMapping("/{compId}")
+    public CompilationDto updateCompilation(@PathVariable Long compId,
+                                            @Valid @RequestBody UpdateCompilationRequest request) {
+        log.info("PATCH /admin/compilations/{} - {}", compId, request);
+        return compilationService.updateCompilation(compId, request);
+    }
+}
