aeb38277977d32d3741df01345d933ac8ae7e6d8
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:02:02 +0300

    M-SV-RQ: Реализован ParticipationRequestDto для запросов на участие

---
+++ main-service/src/main/java/ru/practicum/main/request/dto/ParticipationRequestDto.java
@@ -0,0 +1,17 @@
+package ru.practicum.main.request.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ParticipationRequestDto {
+    Long id;
+    String created;
+    Long event;
+    Long requester;
+    String status;
+}
