d0a5709c4c0ab29dda9c1ea523f2ee48ff15a800
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 17:34:15 +0300

    M-SV-US: Добавлены @Validated в UserController для валидации параметров

--- main-service/src/main/java/ru/practicum/main/user/controller/UserController.java
+++ main-service/src/main/java/ru/practicum/main/user/controller/UserController.java
@@ -3,6 +3,7 @@ package ru.practicum.main.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.main.user.dto.NewUserRequest;
 import ru.practicum.main.user.dto.UserDto;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
 
     private final UserService userService;
