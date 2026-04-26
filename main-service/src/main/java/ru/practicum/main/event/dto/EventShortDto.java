8cf345bff8560c8c9b7bc5d66836dd639689749a
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-26 16:55:16 +0300

    M-SV-EV: Реализован EventShortDto с краткой информацией о событии

---
+++ main-service/src/main/java/ru/practicum/main/event/dto/EventShortDto.java
@@ -0,0 +1,29 @@
+package ru.practicum.main.event.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.main.category.dto.CategoryDto;
+import ru.practicum.main.user.dto.UserShortDto;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EventShortDto {
+    Long id;
+    String annotation;
+    CategoryDto category;
+    Long confirmedRequests;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime eventDate;
+
+    UserShortDto initiator;
+    Boolean paid;
+    String title;
+    Long views;
+}
