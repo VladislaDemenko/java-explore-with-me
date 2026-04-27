b74fcdde8bd4630e28f60b3afe1d49121ce4546f
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-27 09:22:33 +0300

    M-SV-CP: Пути вынесены в переменные

--- main-service/src/main/java/ru/practicum/main/compilation/controller/CompilationAdminController.java
+++ main-service/src/main/java/ru/practicum/main/compilation/controller/CompilationAdminController.java
@@ -13,30 +13,33 @@ import jakarta.validation.Valid;
 
 @Slf4j
 @RestController
-@RequestMapping("/admin/compilations")
+@RequestMapping(CompilationAdminController.BASE_PATH)
 @RequiredArgsConstructor
 public class CompilationAdminController {
+
+    public static final String BASE_PATH = "/admin/compilations";
+    public static final String COMP_ID_PATH = "/{compId}";
 
     private final CompilationService compilationService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CompilationDto createCompilation(@Valid @RequestBody NewCompilationDto newCompilationDto) {
-        log.info("POST /admin/compilations - {}", newCompilationDto);
+        log.info("POST {} - {}", BASE_PATH, newCompilationDto);
         return compilationService.createCompilation(newCompilationDto);
     }
 
-    @DeleteMapping("/{compId}")
+    @DeleteMapping(COMP_ID_PATH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCompilation(@PathVariable Long compId) {
-        log.info("DELETE /admin/compilations/{}", compId);
+        log.info("DELETE {}/{}", BASE_PATH, compId);
         compilationService.deleteCompilation(compId);
     }
 
-    @PatchMapping("/{compId}")
+    @PatchMapping(COMP_ID_PATH)
     public CompilationDto updateCompilation(@PathVariable Long compId,
                                             @Valid @RequestBody UpdateCompilationRequest request) {
-        log.info("PATCH /admin/compilations/{} - {}", compId, request);
+        log.info("PATCH {}/{} - {}", BASE_PATH, compId, request);
         return compilationService.updateCompilation(compId, request);
     }
 }
