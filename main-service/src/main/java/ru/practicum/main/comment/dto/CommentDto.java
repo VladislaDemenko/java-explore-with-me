05f5fe10af276ca0c5e565c574d271e6f7c434b2
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-27 12:14:05 +0300

    M-SV-CM: Добавлены класс-DTO для ответа с данными комментария

---
+++ main-service/src/main/java/ru/practicum/main/comment/dto/CommentDto.java
@@ -0,0 +1,28 @@
+package ru.practicum.main.comment.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CommentDto {
+    Long id;
+    String text;
+    Long eventId;
+    Long authorId;
+    String authorName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime createdOn;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime editedOn;
+
+    Boolean isEdited;
+}
